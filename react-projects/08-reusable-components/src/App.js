7eabdb124e82f4c1dcfa3d1289d81f4c3058642c
Evgeniy Zozulya <zeawork@gmail.com>
2024-11-03 13:41:04 +0200

    207

--- react-projects/08-reusable-components/src/App.js
+++ react-projects/08-reusable-components/src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import MyComponent from './components/MyComponent'
-import OtherComponent from './components/OtherComponent'
+import OtherComponent from "./components/OtherComponent"
 
 function App() {
   return (
